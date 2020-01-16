877d62a2284b30f9c639d7cb7515e2b1a256655a
dododedodonl <github@tomdev.nl>
2020-01-16 17:50:38 +0100

    move frontend to npm scaffolding

--- resources/views/cursor.blade.php
+++ resources/views/cursor.blade.php
@@ -5,26 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
-    <style type="text/css">
-	    .bg-main-dark {
-		    background-color: rgb(34, 34, 34) !important;
-	    }
-	    .bg-new-dark {
-		    background-color: rgb(71, 71, 71) !important;
-		    color: rgb(192, 192, 192) !important;
-	    }
-	    .bg-secondary-dark {
-		    background-color: rgb(51, 51, 51) !important;
-		    color: rgb(192, 192, 192) !important;
-	    }
-	    
-	    .text-muted-dark {
-		    color: rgb(192, 192, 192) !important;
-	    }
-    </style>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>  --}}
+
+    <!-- Style -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
     <title>Cursor Listviewer</title>
   </head>
@@ -58,10 +46,5 @@
         </footer>
     </div>
 
-    {{-- <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> --}}
-  </body>
+    </body>
 </html>
